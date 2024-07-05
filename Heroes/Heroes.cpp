827d0d46b272d788697d87aeb40d457e38bfafe6
vitkenji <vkz.yamada@gmail.com>
2024-07-05 11:18:24 -0300

    SFML installed

--- Heroes/Heroes.cpp
+++ Heroes/Heroes.cpp
@@ -1,4 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()::cout << "Hello World!\n";
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }

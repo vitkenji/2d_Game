fdba55df0d57f40d5f15300749acf95ed7977b8b
vitkenji <vkz.yamada@gmail.com>
2024-07-16 11:48:18 -0300

    class Text

---
+++ Heroes/Text.cpp
@@ -0,0 +1,77 @@
+#include "Text.hpp"
+
+namespace GraphicalElements
+{
+	Managers::GraphicManager* Text::pGraphicManager = Managers::GraphicManager::getInstance();
+
+	Text::Text(Math::CoordinateF position, std::string info, const char* path)
+	{
+		text.setString(info);
+		text.setFont(*pGraphicManager->loadFont(path));
+		text.setPosition(sf::Vector2f(position.x, position.y));
+		text.setCharacterSize(FONT_SIZE);
+		text.setFillColor(sf::Color::White);
+		setTextAlignment(TextAlignment::left);
+
+	}
+
+	Text::~Text()
+	{
+
+	}
+
+	void Text::setTextInfo(std::string info)
+	{
+		this->info = info;
+		text.setString(info);
+	}
+
+	void Text::setPosition(Math::CoordinateF position)
+	{
+		text.setPosition(sf::Vector2f(position.x, position.y));
+	}
+
+	void Text::setTextColor(int r, int g, int b)
+	{
+		text.setFillColor(sf::Color(r, g, b));
+	}
+
+	void Text::setTextAlignment(TextAlignment alignment)
+	{
+		switch (alignment)
+		{
+		case TextAlignment::left:
+			text.setOrigin(0, 0);
+		case TextAlignment::center:
+			text.setOrigin(getSize().x / 2, getSize().y);
+			break;
+		case TextAlignment::right:
+			text.setOrigin(getSize().x, 0);
+			break;
+		default: 
+			text.setOrigin(getSize().x / 2, getSize().y);
+			break;
+		}
+	}
+
+	void Text::render()
+	{
+		pGraphicManager->render(&text);
+	}
+
+	Math::CoordinateF Text::getSize()
+	{
+		sf::FloatRect textRect = text.getLocalBounds();
+		return Math::CoordinateF(textRect.width, textRect.height);
+	}
+
+	std::string Text::getInfo()
+	{
+		return info;
+	}
+
+	Math::CoordinateF Text::getPosition()
+	{
+		return Math::CoordinateF(text.getPosition().x, text.getPosition().y);
+	}
+}
---
+++ Heroes/Text.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include "Animation.hpp"
+#include "GraphicManager.hpp"
+#include "Coordinate.hpp"
+
+namespace GraphicalElements
+{
+	enum TextAlignment
+	{
+		left,
+		center,
+		right
+	};
+
+	class Text
+	{
+	private:
+		std::string info;
+		sf::Text text;
+		static Managers::GraphicManager* pGraphicManager;
+
+	public:
+		Text(Math::CoordinateF position, std::string info, const char* path);
+		~Text();
+
+		void setTextInfo(std::string info);
+		void setPosition(Math::CoordinateF position);
+		void setTextColor(int r, int g, int b);
+		void setFontSize(const int size);
+		void setTextAlignment(TextAlignment aligment);
+		void render();
+		Math::CoordinateF getSize();
+		std::string getInfo();
+		Math::CoordinateF getPosition();
+
+	};
+}

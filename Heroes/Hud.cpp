516f9e1ab4c9a40469c8e93b00245a52a0e7b522
vitkenji <vkz.yamada@gmail.com>
2024-07-24 08:10:07 -0300

    Bug in Hud fixed

--- Heroes/Hud.cpp
+++ Heroes/Hud.cpp
@@ -24,63 +24,24 @@ namespace GraphicalElements
 	{
 		int life = pPlayer->getLife();
 
-		for (int i = 0; i < 6; i++)
+		for (int i = 5; i >= 0; i--)
 		{
-			hearts[i].update(full, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * i, position.y));
+			if (life >= 10000 * (i + 1))
+			{
+				hearts[i].update(full, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * i, position.y));
+			}
+			else if (life >= 10000 * i + 5000)
+			{
+				hearts[i].update(half, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * i, position.y));
+			}
+			else
+			{
+				hearts[i].update(empty, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * i, position.y));
+			}
+			
 
 		}
-		if (life < 60000)
-		{
-			if (life >= 55000)
-			{
-				hearts[5].update(half, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * 5, position.y));
-			}
-			else if (life < 50000)
-			{
-				hearts[5].update(empty, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * 5, position.y));
-				if (life >= 45000)
-				{
-					hearts[4].update(half, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * 4, position.y));
-				}
-				if (life < 40000)
-				{
-					hearts[4].update(empty, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * 4, position.y));
-					if (life >= 35000)
-					{
-						hearts[3].update(half, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * 3, position.y));
-					}
-					if (life < 30000)
-					{
-						hearts[3].update(empty, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * 3, position.y));
-						if (life >= 25000)
-						{
-							hearts[2].update(half, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * 2, position.y));
-						}
-						if (life < 20000)
-						{
-							hearts[2].update(empty, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * 2, position.y));
-							if (life >= 15000)
-							{
-								hearts[1].update(half, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * 1, position.y));
-							}
-							if (life < 10000)
-							{
-								hearts[1].update(empty, Math::CoordinateF(position.x + HEART_SIZE_X * 1.4 * 1, position.y));
-								if (life >= 5000)
-								{
-									hearts[0].update(half, Math::CoordinateF(position.x, position.y));
-								}
-								if (life < 0)
-								{
-									hearts[0].update(empty, Math::CoordinateF(position.x, position.y));
-								}
-							}
-						}
-					}
-				}
-
-			}
-		}
+		
 	}
 
 	void Hud::render()
